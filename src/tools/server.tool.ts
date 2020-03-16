08f87eabccd8cfab09b167f3983e450c7fc57997
Zimtir <zimtir@mail.ru>
2020-03-17 01:09:59 +0300

    drop jwt types

--- src/tools/server.tool.ts
+++ src/tools/server.tool.ts
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import passport from 'passport'
 import session from 'express-session'
-import jsonwebtoken from 'jsonwebtoken'
+import * as jsonwebtoken from 'jsonwebtoken'
 
 import IUser from '../interfaces/user.interface'
 import IJWT from '../interfaces/jwt.interface'
@@ -116,7 +116,7 @@ export default class ServerTool {
         return undefined
       }
 
-      const result = jsonwebtoken.verify(token, config.key, (err, decoded: any) => {
+      const result = jsonwebtoken.verify(token, config.key, (err: any, decoded: any) => {
         if (CommonTool.isNonEmpty(decoded) && !err) return decoded.data
         else return undefined
       })

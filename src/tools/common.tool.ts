f5a45f08b3b57c3919d7afabf9646045e0a925b0
Zimtir <zimtir@mail.ru>
2020-04-03 04:07:45 +0300

    add new functions to common tool

--- src/tools/common.tool.ts
+++ src/tools/common.tool.ts
@@ -12,13 +12,92 @@ export default class CommonTool {
 
     if (arr) {
       if (arr.length >= 0) {
-        arr.map(a => {
+        arr.map((a) => {
           if (CommonTool.isEmpty(a)) {
             output = false
           }
         })
       } else output = false
     } else output = false
+
+    return output
+  }
+
+  static toogle = (tag: string, className: string) => {
+    let element = document.querySelector(tag)
+    if (element) {
+      element.classList.add(className)
+    }
+  }
+
+  static getJwt = () => {
+    return CommonTool.getCookie('jwt')
+  }
+
+  static getClickAction = (id: string) => {
+    let element = CommonTool.getElement(id)
+    if (element) {
+      return element.click
+    } else return () => {}
+  }
+
+  static parseJwt = (token: string) => {
+    try {
+      return JSON.parse(atob(token.split('.')[1]))
+    } catch (e) {
+      return null
+    }
+  }
+
+  static getCookie = (name: string) => {
+    try {
+      let value = ' ' + document.cookie
+      let parts = value.split(' ' + name + '=')
+      if (parts && parts.length == 2) {
+        return parts.pop()!.split('').shift()
+      } else {
+        return ''
+      }
+    } catch (err) {
+      return ''
+    }
+  }
+
+  static getInput = (name: string): HTMLInputElement => {
+    return <HTMLInputElement>CommonTool.getElement(name)
+  }
+
+  static getElement = (name: string): HTMLElement => {
+    return <HTMLElement>document.getElementById(name)
+  }
+
+  static isPage = (path: string) => {
+    return document.location.href.includes(path)
+  }
+
+  static getUrlAttributes = () => {
+    let output: string[] = []
+
+    let self = window.location.toString()
+    let querystring = self.split('?')
+    if (querystring.length > 1) {
+      output = querystring[1].split('&')
+    }
+
+    return output
+  }
+
+  getUrlAttrVal = (name: string) => {
+    let output = null
+    let attrs = CommonTool.getUrlAttributes()
+
+    for (let attr in attrs) {
+      let keyval = attrs[attr].split('=')
+      if (keyval[0] === name) {
+        output = keyval[1]
+        break
+      }
+    }
 
     return output
   }
